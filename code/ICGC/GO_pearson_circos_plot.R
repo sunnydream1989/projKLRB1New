24bae8d4dd0dc14e378f75ed47e2de4f43eec3a8
MFX-HOME\WJF <512537531@qq.com>
2024-07-21 21:46:33 +0800

    update

---
+++ code/ICGC/GO_pearson_circos_plot.R
@@ -0,0 +1,54 @@
+rm(list=ls())
+
+library(dplyr)
+library(stringr)
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+clinical_data<-read.delim('../../data/ICGC-LIRI-JP/clean/ICGC-LIRI-JP_clinical_clean_include_normal.csv', sep=',', check.names = F)
+clinical_data<-clinical_data %>% 
+  filter(str_ends(submitted_sample_id, "_Cancer"))
+
+data<-read.delim('../../data/ICGC-LIRI-JP/clean/ICGC-LIRI-JP_rna_tpm_clean_50_include_normal.csv',row.names=1, sep=',', check.names = F)
+data<-data[,clinical_data$icgc_sample_id]
+non_na_columns <- !is.na(data["KLRB1", ]) # 有一个KLRB1是NA，需要剔除
+data <- data[, non_na_columns]
+
+clinical_data = clinical_data[clinical_data$icgc_sample_id %in% colnames(data), ]
+data<-data[,clinical_data$icgc_sample_id]
+
+sample_id = colnames(data)
+data = log2(data + 1)###########################################################
+
+gene_list = c("KLRB1", "PDCD1", "CD274", "HAVCR2", "LAG3", "CTLA4", "TIGIT", "CD47", "CD200R1")
+gene_list2 = c("KLRB1", "PD1", "PDL1", "TIM-3", "LAG3", "CTLA4", "TIGIT", "CD47", "CD200R1")
+
+dat <- data[gene_list, ]
+
+k = c()
+R = rownames(dat)
+for (k in 1:length(rownames(dat))){
+  r = c()
+  for (i in 1: length(rownames(dat))){
+    a = as.numeric(dat[k, ])
+    b = as.numeric(dat[i, ])
+    r = c(r, cor(a, b, use="complete.obs"))
+  }
+  R = cbind(R, r)
+}
+
+length2<-length(R[1, ])
+colnames(R)[1] = 'gene'
+colnames(R)[2:length2] <- gene_list2
+R[,1] = gene_list2
+
+write.table(R, '../../data/GO/ICGC/Pearson_matrix_v2.txt', sep = '\t', quot = F, row.names = F)
+
+R = read.delim('../../data/GO/ICGC//Pearson_matrix_v2.txt', row.names = 1)
+dat = data.matrix(R)
+
+require(corrgram)
+
+# png('GO/perarson_matrix_plot.png')
+pdf("../../data/GO/ICGC//perarson_matrix_plot.pdf")
+corrgram(dat, order=F, lower.panel=panel.cor, upper.panel=panel.pie, text.panel=panel.txt,main="Correlogram of phenotype correlated markers", col.regions=colorRampPalette(c("green1", "white","firebrick1")))
+dev.off()
---
+++ code/TCGA-LIHC/GO_pearson_circos_plot.R
@@ -0,0 +1,45 @@
+rm(list=ls())
+
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+data<-read.delim('../../data/TCGA-LIHC/clean/TCGA-LIHC_tpm_protein_coding_clean.csv',row.names=1, sep=',', check.names = F)
+
+sample_id = colnames(data)
+is_cancer = sapply(sample_id, function(x) substr(x, 14, 14) == '0')
+data = data[,is_cancer]
+
+data = log2(data + 1)###########################################################
+
+gene_list = c("KLRB1", "PDCD1", "CD274", "HAVCR2", "LAG3", "CTLA4", "TIGIT", "CD47", "CD200R1")
+gene_list2 = c("KLRB1", "PD1", "PDL1", "TIM-3", "LAG3", "CTLA4", "TIGIT", "CD47", "CD200R1")
+
+dat <- data[gene_list, ]
+
+k = c()
+R = rownames(dat)
+for (k in 1:length(rownames(dat))){
+  r = c()
+  for (i in 1: length(rownames(dat))){
+    a = as.numeric(dat[k, ])
+    b = as.numeric(dat[i, ])
+    r = c(r, cor(a, b))
+  }
+  R = cbind(R, r)
+}
+
+length2<-length(R[1, ])
+colnames(R)[1] = 'gene'
+colnames(R)[2:length2] <- gene_list2
+R[,1] = gene_list2
+
+write.table(R, '../../data/GO/TCGA-LIHC/Pearson_matrix_v2.txt', sep = '\t', quot = F, row.names = F)
+
+R = read.delim('../../data/GO/TCGA-LIHC/Pearson_matrix_v2.txt', row.names = 1)
+dat = data.matrix(R)
+
+require(corrgram)
+
+# png('GO/perarson_matrix_plot.png')
+pdf("../../data/GO/TCGA-LIHC/perarson_matrix_plot.pdf")
+corrgram(dat, order=F, lower.panel=panel.cor, upper.panel=panel.pie, text.panel=panel.txt,main="Correlogram of phenotype correlated markers", col.regions=colorRampPalette(c("green1", "white","firebrick1")))
+dev.off()
