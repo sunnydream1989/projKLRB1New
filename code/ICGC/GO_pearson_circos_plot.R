57627cbbd49a00871d3d5265ffb866fc6f579a5b
feixiangdream <512537531@qq.com>
2024-07-24 21:05:49 +0800

    update

--- code/ICGC/GO_pearson_circos_plot.R
+++ code/ICGC/GO_pearson_circos_plot.R
@@ -20,9 +20,11 @@ sample_id = colnames(data)
 data = log2(data + 1)###########################################################
 
 gene_list = c("KLRB1", "PDCD1", "CD274", "HAVCR2", "LAG3", "TIGIT", "CD200R1")
-gene_list2 = c("KLRB1", "PD1", "PDL1", "TIM3", "LAG3", "TIGIT", "CD200R1")
+gene_list2 = c("CD161", "PD1", "PDL1", "TIM3", "LAG3", "TIGIT", "CD200R1")
 
 dat <- data[gene_list, ]
+dat2 <- data.frame(t(dat))
+colnames(dat2) = gene_list2
 
 k = c()
 R = rownames(dat)
@@ -44,12 +46,55 @@ R[,1] = gene_list2
 write.table(R, '../../data/GO/ICGC/Pearson_matrix_v2.txt', sep = '\t', quot = F, row.names = F)
 
 R = read.delim('../../data/GO/ICGC//Pearson_matrix_v2.txt', row.names = 1)
-dat = data.matrix(R)
+cm = data.matrix(R)
 
 require(corrgram)
 
 # png('GO/perarson_matrix_plot.png')
-pdf("../../data/GO/ICGC//perarson_matrix_plot.pdf")
-corrgram(dat, order=F, lower.panel=panel.cor, upper.panel=panel.pie, text.panel=panel.txt,main="ICGC", col.regions=colorRampPalette(c("firebrick1", "white","blue")))
+pdf("../../data/GO/ICGC/perarson_matrix_plot.pdf")
+corrgram(cm, order=F, lower.panel=panel.cor, upper.panel=panel.pie, text.panel=panel.txt,main="ICGC", col.regions=colorRampPalette(c("firebrick1", "white","blue")))
 dev.off()
 
+################################################################################
+dat2[is.na(dat2)] = 0
+corr <- cor(dat2)
+res <- cor.mtest(dat2, conf.level = .95)
+p <- res$p
+
+pdf("../../data/GO/ICGC/perarson_matrix_plot2.pdf")
+
+mycol <- colorRampPalette(c("#06a7cd", "white", "#e74a32"), alpha = TRUE)
+# mycol <- colorRampPalette(c("#0AA1FF","white", "#F5CC16"), alpha = TRUE)
+#上三角图添加显著性水平星号：
+corrplot(corr, method = c('pie'), 
+         type = c('upper'), 
+         col = mycol(100),
+         outline = 'grey', 
+         order = c('original'),
+         diag = TRUE,
+         tl.cex = 1, 
+         tl.col = 'black',
+         tl.pos = 'd',
+         p.mat = p,
+         sig.level = c(.001, .01, .05),
+         insig = "label_sig", #显著性标注样式："pch", "p-value", "blank", "n", "label_sig"
+         pch.cex = 1.2, #显著性标记大小
+         pch.col = 'black' #显著性标记颜色
+)
+#下三角图添加不显著叉号：
+corrplot(corr, add = TRUE,
+         method = c('number'), 
+         type = c('lower'),
+         col = mycol(100),
+         order = c('original'),
+         diag = FALSE, 
+         number.cex = 0.9,
+         tl.pos = 'n', 
+         cl.pos = 'n',
+         p.mat = p,
+         insig = "pch"
+)
+
+# title(main = "TCGA", cex.main = 1.5, font.main = 2)
+
+dev.off()
--- code/TCGA-LIHC/GO_pearson_circos_plot.R
+++ code/TCGA-LIHC/GO_pearson_circos_plot.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 
+library(corrplot)
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 data<-read.delim('../../data/TCGA-LIHC/clean/TCGA-LIHC_tpm_protein_coding_clean.csv',row.names=1, sep=',', check.names = F)
@@ -11,9 +12,11 @@ data = data[,is_cancer]
 data = log2(data + 1)###########################################################
 
 gene_list = c("KLRB1", "PDCD1", "CD274", "HAVCR2", "LAG3", "TIGIT", "CD200R1")
-gene_list2 = c("KLRB1", "PD1", "PDL1", "TIM3", "LAG3", "TIGIT", "CD200R1")
+gene_list2 = c("CD161", "PD1", "PDL1", "TIM3", "LAG3", "TIGIT", "CD200R1")
 
 dat <- data[gene_list, ]
+dat2 <- data.frame(t(dat))
+colnames(dat2) = gene_list2
 
 k = c()
 R = rownames(dat)
@@ -35,11 +38,54 @@ R[,1] = gene_list2
 write.table(R, '../../data/GO/TCGA-LIHC/Pearson_matrix_v2.txt', sep = '\t', quot = F, row.names = F)
 
 R = read.delim('../../data/GO/TCGA-LIHC/Pearson_matrix_v2.txt', row.names = 1)
-dat = data.matrix(R)
+cm = data.matrix(R)
 
 require(corrgram)
 
 # png('GO/perarson_matrix_plot.png')
 pdf("../../data/GO/TCGA-LIHC/perarson_matrix_plot.pdf")
-corrgram(dat, order=F, lower.panel=panel.cor, upper.panel=panel.pie, text.panel=panel.txt,main="TCGA", col.regions=colorRampPalette(c("green1", "white","firebrick1")))
+corrgram(cm, order=F, lower.panel=panel.cor, upper.panel=panel.pie, text.panel=panel.txt,main="TCGA", col.regions=colorRampPalette(c("green1", "white","firebrick1")))
+dev.off()
+
+################################################################################
+corr <- cor(dat2)
+res <- cor.mtest(dat2, conf.level = .95)
+p <- res$p
+
+pdf("../../data/GO/TCGA-LIHC/perarson_matrix_plot2.pdf")
+
+mycol <- colorRampPalette(c("#06a7cd", "white", "#e74a32"), alpha = TRUE)
+# mycol <- colorRampPalette(c("#0AA1FF","white", "#F5CC16"), alpha = TRUE)
+#上三角图添加显著性水平星号：
+corrplot(corr, method = c('pie'), 
+         type = c('upper'), 
+         col = mycol(100),
+         outline = 'grey', 
+         order = c('original'), 
+         diag = TRUE,
+         tl.cex = 1, 
+         tl.col = 'black',
+         tl.pos = 'd',
+         p.mat = p,
+         sig.level = c(.001, .01, .05),
+         insig = "label_sig", #显著性标注样式："pch", "p-value", "blank", "n", "label_sig"
+         pch.cex = 1.2, #显著性标记大小
+         pch.col = 'black' #显著性标记颜色
+)
+#下三角图添加不显著叉号：
+corrplot(corr, add = TRUE,
+         method = c('number'), 
+         type = c('lower'),
+         col = mycol(100),
+         order = c('original'),
+         diag = FALSE, 
+         number.cex = 0.9,
+         tl.pos = 'n', 
+         cl.pos = 'n',
+         p.mat = p,
+         insig = "pch"
+)
+
+# title(main = "TCGA", cex.main = 1.5, font.main = 2)
+
 dev.off()
